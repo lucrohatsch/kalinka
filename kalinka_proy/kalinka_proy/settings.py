d80f0f8e3daa517bcaaf2d32ae16df80085cc752
luc <lucas.rohatsch@gmail.com>
2022-02-11 22:12:31 -0300

    kalinka 1.0

--- kalinka_proy/kalinka_proy/settings.py
+++ kalinka_proy/kalinka_proy/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# ----------environ-------------
+env = environ.Env()
+environ.Env.read_env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ynihg1(d&5jl#tawok^xjl^*tjheo^4v5jk6%gidxzxaek^$ce'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -89,8 +95,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kalinka',
-        'USER': 'kalinka',
-        'PASSWORD': 'kalinka',
+        'USER': env.str('USER'),
+        'PASSWORD': env.str('PASSWORD'),
         'HOST': 'pgdb',
         'PORT': 5432,
     }
